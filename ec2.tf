a755114d7d1959a4d4073ef3f790a497e6d30f27
pavel-saladukha <pavel.soloduha@outlook.com>
2024-09-01 14:26:16 +0200

    Added missing tag

--- ec2.tf
+++ ec2.tf
@@ -74,6 +74,12 @@ resource "aws_autoscaling_group" "this" {
     }
   }
 
+  tag {
+    key                 = "Name"
+    value               = "vw-asg"
+    propagate_at_launch = true
+  }
+
   lifecycle {
     create_before_destroy = true
   }
